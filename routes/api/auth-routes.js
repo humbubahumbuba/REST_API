4952e9396963507cbc2482c88dfe4a90f153d3d8
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-06-17 23:15:47 +0300

    bugfix_auth

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -1,5 +1,16 @@
 const express = require('express');
 
+const { validateBody } = require('../../middlewares');
+const { schemas } = require('../../models/user');
+
+const usersController = require('../../controllers/users-controller');
+
 const router = express.Router();
+
+router.post(
+  '/register',
+  validateBody(schemas.registerSchema),
+  usersController.register
+);
 
 module.exports = router;
