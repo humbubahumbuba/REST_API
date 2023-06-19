8186a7049ea75b6ce3cf98c27da6a83dddfce370
Paul Droshniev <108367655+humbubahumbuba@users.noreply.github.com>
2023-06-19 16:33:26 +0300

    logout message

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -63,7 +63,7 @@ const logOut = async (req, res, next) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: '' });
   res.status(204).json({
-    message: 'Log Out succesfully',
+    message: 'No Content',
   });
 };
 
